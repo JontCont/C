65a957b389cc922cc70eb0149b9c7fb53ade2c74
JontCont <information9527@gmail.com>
2020-01-07 21:30:36 +0800

    APCS(105/03/05) TO 109/01/07 DONE

--- APCS/_Transformation.c
+++ APCS/_Transformation.c
@@ -1,62 +1,69 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int a[10][10],b[10][10];
-void rotate(int r,int c)
+int a[10][10]={{0}};
+int b[10][10]={{0}};
+int temp=0;
+void rotate(int R,int C)
 {
-	int i ,j;
-	for(i=c-1;i>=0;i++)
-		for(j=0;j<r;j++)
-			b[(c-1)-i][j]=a[j][i];
-	for(i=0; i<c;i++)
-		for(j=0;j<r;j++)
-			a[i][j]=b[i][j];
+    for(int x=0;x<R;++x)
+    {
+        for(int y=0;y<C;++y)
+            b[x][y]=a[y][R-x-1];
+    }
+    for(int x=0;x<R;++x)
+    {
+        for(int y=0;y<C;++y)
+            a[x][y]=b[x][y];        
+    }
 }
 
-void mirror(int r,int c)
+void mirror(int R,int C)
 {
-	int i,j;
-	for(i=r-1;i>=0;i++)
-		for(j=0;j<c;j++)
-			b[(r-1)-i][j]=a[j][i];
-	for(i=0; i<r;i++)
-		for(j=0;j<c;j++)
-			a[i][j]=b[i][j];
+    for(int x=0;x<R/2;++x)
+    {
+        for(int y=0;y<C;++y)
+        {
+            temp=a[x][y];
+            a[x][y]=a[R-x-1][y];
+            a[R-x-1][y]=temp;
+        }
+    }
 }
+
 int main()
 {
-	int op[10],R,C,M,temp;
-	while (printf("%d,%d,%d",R,C,M))
-	{
-		for(int i=0;i<R;i++)
-		{
-			for(int j=0;j<C;j++)
-				printf("%d",a[i][j]);
-		}
-		for(int i=0;i<M;i++) printf("%d",op[i]);
-		for(int i=M-1;i>=0;i--){
-			if (op[i] == 1) 
-				mirror(R,C);
-			else{
-				rotate(R,C);
-				temp=R;
-				R=C;
-				C=temp;
-			}
-		}
-		scanf("%d, %d\n",R,C); 
+    int R,C,M;
+    int i,j;
+    int option[10]={0};
+    scanf("%d %d %d",&R,&C,&M);
+    for(i=0;i<R;++i)
+        for(j=0;j<C;++j)    
+            scanf(" %d",&a[i][j]);
+    //-------------------------//
+    for(i=0;i<M;i++)
+        scanf(" %d",&option[i]);     
+    //---------------------//
+    for(i=M-1;i>=0;i--)
+    {
+        if(option[i]==0) 
+        {   
+            temp=C;C=R;R=temp;
+            rotate(R,C);
+        }
+        else  mirror(R,C);   
+    }
+    //-----------------------//
+        printf("\n%d %d",R,C);
 
-	    for(int i=0;i<R;i++)
-		{
-		    printf("%d",a[i][0]);
-		    for(int j=1;j<C;j++)
-			    printf("%d",a[i][j]);
-	    }	
-	}
-	
+    for(int x=0;x<R;++x)
+    {
+        printf("\n");
+        for(int y=0;y<C;++y)
+        {
+            if(y!=0)printf(" ");
+            printf("%d",a[x][y]);
+        }
+    }
+    
 }
-
-
- 
-
