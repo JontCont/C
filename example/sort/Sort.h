d18fd8cc9a4f11804a38700cf2de9c57b0048900
JontCont <information9527@gmail.com>
2020-04-28 08:34:01 +0800

    04/28

--- example/sort/Sort.h
+++ example/sort/Sort.h
@@ -1,4 +1,4 @@
-
+void swap(int *A, int *B);
 void SelSort(int A[], int n);
 void SelSort_data(int A[], int n);
 
@@ -7,6 +7,6 @@ void BubSort_data(int A[], int n);
 
 void InSort(int A[], int n);
 void InSort_data(int A[], int n);
-  
-int RandomNum(int A[],int Num);
-void PrintSort(int A[], int n); 
+
+int RandomNum(int A[], int Num);
+void PrintSort(int A[], int n);
